426a1e99949c17e0dd4b885e07175159747615de
rahulnitc3003 <kumar_rahul@teknotrait.com>
2019-04-16 19:26:23 +0530

    edge npm template added

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// const port = 9000;
+
+// app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//     console.log(path.resolve(__dirname, 'pages/index.html'));
+//     res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+// });
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+// });
+// app.get('/contact', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+// });
+
+// app.listen(port, () => console.log(`App is running on port ${port}`));
+
+
+
 const express = require('express');
 const path = require('path');
+const expressEdge = require('express-edge');
 
 const app = express();
 const port = 9000;
 
 app.use(express.static('public'));
+app.use(expressEdge);
+
+app.set('views', `${__dirname}/views`);
 
 app.get('/', (req, res) => {
-    console.log(path.resolve(__dirname, 'pages/index.html'));
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    res.render('about');
 });
+
+app.get('/post', (req, res) => {
+    res.render('post');
+});
+
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render('contact');
 });
+
+
 
 app.listen(port, () => console.log(`App is running on port ${port}`));
